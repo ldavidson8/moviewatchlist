d3fd2bb97764ee42617a23e807c97d922f5f17e0
lewisdavidson <lewisdavidson8@gmail.com>
2022-03-21 18:42:18 +0000

    MovieCard title and hover

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,19 +2,24 @@ import { MovieControls } from "./MovieControls";
 
 export const MovieCard = ({ movie, type }) => {
   return (
-    <div className="w-full rounded-md overflow-hidden relative block group">
-      <div className="absolute top-0 left-0 w-full h-full border-2 border-transparent group-hover:border-teal-200 rounded-md"></div>
-      {movie.poster_path ? (
-        <img
-          src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
-          alt={`${movie.title} Poster`}
-          className="w-full block rounded-md"
-        />
-      ) : (
-        <div className="w-16 h-28 bg-gray-400 mr-4 rounded-md block text-transparent"></div>
-      )}
+    <>
+      <div className="group relative block w-full overflow-hidden rounded-md">
+        <div className="absolute top-0 left-0 h-full w-full rounded-md border-2 border-transparent group-hover:border-teal-200 group-hover:bg-gray-700 group-hover:opacity-75 group-hover:transition-all group-hover:duration-300"></div>
+        {movie.poster_path ? (
+          <img
+            src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+            alt={`${movie.title} Poster`}
+            className="block w-full rounded-md"
+          />
+        ) : (
+          <div className="mr-4 block h-[340px] w-full rounded-md bg-gray-500"></div>
+        )}
+        <h2 className="absolute top-1/2 left-2 text-xl opacity-0 duration-200 group-hover:opacity-100 group-hover:transition-opacity group-hover:delay-150">
+          {movie.title}
+        </h2>
 
-      <MovieControls type={type} movie={movie} />
-    </div>
+        <MovieControls type={type} movie={movie} />
+      </div>
+    </>
   );
 };
