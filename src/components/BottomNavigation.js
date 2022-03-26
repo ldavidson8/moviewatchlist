5087b0f52c1f22c2710b5aa320ef973111c899bc
lewisdavidson <lewisdavidson8@gmail.com>
2022-03-26 07:10:01 +0000

    Active Link styling

--- src/components/BottomNavigation.js
+++ src/components/BottomNavigation.js
@@ -1,34 +1,55 @@
 import HomeIcon from "@mui/icons-material/Home";
 import MovieIcon from "@mui/icons-material/Movie";
 import AddIcon from "@mui/icons-material/Add";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export const BottomNav = () => {
   return (
     <>
       <div className="fixed bottom-0 h-[80px] w-full bg-gray-700 text-gray-100 shadow-xl dark:bg-gray-700 dark:text-gray-100 sm:hidden">
         <div className="grid h-full grid-cols-3">
-          <Link
+          <NavLink
             to="/"
-            className="flex h-full flex-col items-center justify-center border-r-2 border-gray-600"
+            className={({ isActive }) =>
+              [
+                "flex h-full flex-col items-center justify-center border-r-2 border-gray-600",
+                isActive ? "text-teal-200" : null,
+              ]
+                .filter(Boolean)
+                .join(" ")
+            }
           >
             <HomeIcon />
             Home
-          </Link>
-          <Link
+          </NavLink>
+          <NavLink
             to="/watched"
-            className="flex h-full flex-col items-center justify-center border-r-2 border-gray-600"
+            className={({ isActive }) =>
+              [
+                "flex h-full flex-col items-center justify-center border-r-2 border-gray-600",
+                isActive ? "text-teal-200" : null,
+              ]
+                .filter(Boolean)
+                .join(" ")
+            }
           >
             <MovieIcon />
             Watched
-          </Link>
-          <Link
+          </NavLink>
+          <NavLink
             to="/add"
-            className="flex h-full flex-col items-center justify-center border-gray-600"
+            className={({ isActive }) =>
+              [
+                "flex h-full flex-col items-center justify-center",
+                isActive ? "text-teal-200" : null,
+              ]
+                .filter(Boolean)
+                .join(" ")
+            }
           >
             <AddIcon />
             Add
-          </Link>
+          </NavLink>
         </div>
       </div>
     </>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import PlayCircleOutlineIcon from "@mui/icons-material/PlayCircleOutline";
 
 export const Navbar = () => {
@@ -7,25 +7,39 @@ export const Navbar = () => {
       <div className="mx-auto flex h-[75px] max-w-screen-xl items-center">
         <nav className="flex w-full justify-between px-12 xl:p-0">
           <div className="text-2xl">
-            <Link to="/">
+            <NavLink to="/">
               <PlayCircleOutlineIcon />
               WatchList
-            </Link>
+            </NavLink>
           </div>
           <ul className="hidden w-1/2 items-center justify-between sm:flex">
             <li>
-              <Link to="/">Watch List</Link>
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive ? "text-teal-200" : null
+                }
+              >
+                Watch List
+              </NavLink>
             </li>
             <li>
-              <Link to="/watched">Watched</Link>
+              <NavLink
+                to="/watched"
+                className={({ isActive }) =>
+                  isActive ? "text-teal-200" : null
+                }
+              >
+                Watched
+              </NavLink>
             </li>
             <li>
-              <Link
+              <NavLink
                 to="/add"
                 className="rounded-md bg-teal-400 py-3 px-8 text-lg hover:bg-teal-200"
               >
                 Add
-              </Link>
+              </NavLink>
             </li>
           </ul>
         </nav>
